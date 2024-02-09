4d81f4b4f5adcb1339c14b6df7a4d548f3903455
Max Jeon <maxjeon97@gmail.com>
2024-02-09 15:55:42 -0800

    finished average pair and can construct problem

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,55 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/**
+ * constraints: time complexity: O(n)
+ *
+ * test cases:
+ * averagePair([1, 2, 3], 2.5);         // true
+ * averagePair([3, 3, 6, 12, 19], 8);   // false
+ * averagePair([1, 2, 3], 2);           // true
+ * averagePair([], 4);                  // false
+ *
+ * create a calcAverage helper function
+ *
+ * use multiple pointers
+ * assign left and right variables to the beginning and ends of the input array
+ * using a while loop as long as left < right
+ *  average the numbers at the left and right indexes and check to see if
+ *  greater than or equal to the targetAvg
+ *  if less than, move left pointer
+ *  if greater, move right pointer
+ *  if calculated avg = targetAvg
+ *    return true
+ * return false if loop has concluded
+ */
+
+
+/** given a sorted array of numbers, nums, and a numeric target, targetAvg,
+ * determine if there is a pair of numbers in nums where the average of the pair
+ * equals targetAvg. There may be more than one pair.
+ * returns a boolean value
+ */
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while(left < right) {
+    let average = calcAverage(nums[left], nums[right]);
+    if(average === targetAvg) {
+      return true;
+    }
+    else if(average < targetAvg) {
+      left++;
+    }
+    else if(average > targetAvg) {
+      right--;
+    }
+  }
+
+  return false;
+}
+
+/** given two numbers, return the mathematical average of them */
+function calcAverage(num1, num2) {
+  return (num1 + num2) / 2;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,53 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/** test cases:
+ * canConstructWord("aa", "abc") => false
+ * canConstructWord("abc", "dcba") => true
+ * canConstructWord("aabb", "bcabad") => true
+ *
+ * create frequency counter helper function
+ *
+ * within conductor function create two frequency counters, one for each input
+ * iterate through the word frequency counter and for each letter (key),
+ * check to see if the corresponding letters frequency counter key contains
+ * enough amount to create the word
+ * */
+
+/** given two strings, word and letters, return true if the word can be
+ * built from the letters; otherwise return false
+ * there are only lowercase letters so no need to worry about case
+ *
+ * Constraints: time complexity must be O(n)
+*/
+function canConstructWord(word, letters) {
+  if(word.length > letters.length) {
+    return false;
+  }
+  const wordFreqs = getLetterFrequency(word);
+  const lettersFreqs = getLetterFrequency(letters);
+  for(let letter in wordFreqs) {
+    //check to see if lettersFreqs contains the letter in the first place
+    if(lettersFreqs[letter] === undefined) {
+      return false;
+    }
+    //since we know that the letter exists within both counters, check to see
+    //  whether or not lettersFreqs has enough of that letter to construct
+    //  the word
+    if(wordFreqs[letter] > lettersFreqs[letter]) {
+      return false;
+    }
+  }
+  //if all conditions pass within for loop, return true
+  return true;
+}
+
+/** given a string, create a frequency counter object containing the
+ * letters and the number of times they appear as key-value pairs
+ */
+function getLetterFrequency(string) {
+  const letterFrequency = {};
+  for(let letter of string) {
+    const count = letterFrequency[letter] || 0;
+    letterFrequency[letter] = count + 1;
+  }
+  return letterFrequency;
 }
