5172fbb9f2db6114c3b4d485cceb3b51dc5ed02d
Jack Norquist <jack.norquist@outlook.com>
2024-02-09 18:27:24 -0600

    Same Frequence and Two Array Object Problem

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,60 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+/**
+ *Given two postive integers find out if the two numbers have the same frequency
+ * of digits
+ */
+
+/**Test Cases:
+ * sameFrequency(123, 321);   // true
+   sameFrequency(34, 14);     // false
+   sameFrequency(22, 22);     // true
+   sameFrequency(22, 222);    // false
+
+ */
+
+
+/**Get the frequencies ofeach number.
+ *
+ * controller function sets variables equal to the two inputs called on the set
+ * frequncy function. Iterate through first number checks to see if it is in
+ * and checks to see if it equals the same
+ *
+ * false safe makes sure lengths are same
+ */
+
+
+/**Takes two numbers, sets variable equal to the frequency of each number by
+ * using the getNumberFrequency function. Compares the two variables to see if
+ * the frequency is the same. Returns false if they do not, true if they do.
+ */
+function sameFrequency(num1, num2) {
+  if (num1.toString().length !== num2.toString().length) {
+    return false;
+  }
+
+  const numOneFrequency = getNumberFrequency(num1);
+  const numTwoFrequency = getNumberFrequency(num2);
+
+  for (let num in numOneFrequency) {
+    if (!(num in numTwoFrequency)) {
+      return false;
+    }
+    if (numOneFrequency[num] !== numTwoFrequency[num]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/** Takes a number, turns it into a string, goes through the string and
+ * adds the frequency of digits to an object. Returns the object
+*/
+function getNumberFrequency(num) {
+  const stringNumber = num.toString();
+  const numberFrequency = {};
+  for (let num of stringNumber) {
+    let currentNum = numberFrequency[num] || 0;
+    numberFrequency[num] = currentNum + 1;
+  }
+  return numberFrequency;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,53 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**This should take two arrays of possibly-different lengths.
+/The first array consists of keys and the second one consists of values.
+/This should return an object created from the keys and values. If there are not
+enough values, the rest of keys should have a value of null. If there not enough
+ keys, just ignore the rest of values.
+*/
+
+/**
+ * Test Cases:
+ * twoArrayObject(['x', 'y', 'z'], [1, 2]);
+  // {'x': 1, 'y': 2, 'z': null}
+
+  twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]);
+  // {'a': 1, 'b': 2, 'c': 3}
+ */
+
+
+/**
+ * Sudo: Go through the first array add the element at that index to the object
+ * as a key add the element at the same index from the second array as the value
+ * for the corresponding key
+ *
+ * if the furst array is empty return empty obj
+ */
+
+
+
+
+
+
+/**Takes two arrays and returns an object containing the elements of the first
+ * array as the keys and the correspding elementd of the second array as the
+ * values.If there are not enough values, the rest of keys should have a value
+ * of null. If there not enough keys, just ignore the rest of values.
+ */
+function twoArrayObject(keys, values) {
+  if (keys.length === 0) {
+    return {};
+  }
+
+  let arrayPairs = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (values[i] === undefined) {
+      arrayPairs[keys[i]] = null;
+    } else {
+      arrayPairs[keys[i]] = values[i];
+    }
+  }
+  return arrayPairs;
+
 }
+
